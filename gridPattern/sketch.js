a39ac8eb893574d6e0350f5d94f2f17d84bd78e4
Jean-Marc Pelletier <jmp@jmpelletier.com>
2022-05-10 14:34:12 +0900

    配列からランダムに要素を選ぶ

--- gridPattern/sketch.js
+++ gridPattern/sketch.js
@@ -1,6 +1,11 @@
 // ここにスケッチ名を指定することができる。
 let sketchName = '綺麗な絵';
 
+function pickRandom(array) {
+    let i = Math.floor(Math.random() * array.length);
+    return array[i];
+}
+
 // ロードが完了した時に実行する関数。ここで初期化の処理を書く。
 // P5.jsやProcessingのsetupと同じ役割。
 window.addEventListener('load', function() {
@@ -13,9 +18,30 @@ window.addEventListener('load', function() {
         strokeWidth: 1 // 線の幅
     };
 
+    let randomRange = 5;
+
+    let colors = [
+        '#1F363D',
+        '#40798C',
+        '#70A9A1',
+        '#9EC1A3',
+        '#CFE0C3'
+    ];
+
+    let sizes = [
+        5,
+        10,
+        15
+    ];
+
     for (let y = 0; y < view.viewSize.height; y += 20) {
         for (let x = 0; x < view.viewSize.width; x += 20) {
-            Path.Circle([x, y], 5);
+            let rand_x = Math.random() * randomRange - randomRange * 0.5;
+            let rand_y = Math.random() * randomRange - randomRange * 0.5;
+
+            let circle = Path.Circle([x + rand_x, y + rand_y], pickRandom(sizes));
+
+            circle.fillColor = pickRandom(colors);
         }
     }
 
